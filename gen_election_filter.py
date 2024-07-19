d32c200cda9ac4659e14f45dea8b5705e7e333c6
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-07-19 10:33:31 +0000

    Add 2024 data

--- gen_election_filter.py
+++ gen_election_filter.py
@@ -7,7 +7,8 @@ from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 csv_files = {
     '2015': 'data/candidate-level-results-general-election-07-05-2015.csv',
     '2017': 'data/candidate-level-results-general-election-08-06-2017.csv',
-    '2019': 'data/candidate-level-results-general-election-12-12-2019.csv'
+    '2019': 'data/candidate-level-results-general-election-12-12-2019.csv',
+    '2024': 'data/candidate-level-results-general-election-04-07-2024.csv'
 }
 
 
