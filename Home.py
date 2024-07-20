f306c3b1200d7c6bae633d7eb320e080a91a2065
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-07-20 21:29:35 +0000

    Added homepage

---
+++ Home.py
@@ -0,0 +1,8 @@
+# This file can be empty or contain general app configuration
+import streamlit as st
+
+st.title("UK General Election data")
+st.write("I've taken UK General Election data from 2015, 2017, 2019 and 2024 and created a few different ways to view the data.")
+st.write("The data is grouped by political party and shows the total number of votes each party received in each election.")
+st.write("You can view the data in a table or a chart. The charts show all votes cast and so are particularly useful for comparing how well parties have done - independant of the first past the post system.")
+st.write("See the left sidebar for the different pages.")
