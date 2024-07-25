51ad1b589f5705add7ceb368322c8f3de57690a2
Pete Houghton <pete@investigatingsoftware.co.uk>
2024-07-25 19:01:10 +0000

    Add homepage links

--- Home.py
+++ Home.py
@@ -6,3 +6,6 @@ st.write("I've taken UK General Election data from 2015, 2017, 2019 and 2024 and
 st.write("The data is grouped by political party and shows the total number of votes each party received in each election.")
 st.write("You can view the data in a table or a chart. The charts show all votes cast and so are particularly useful for comparing how well parties have done - independant of the first past the post system.")
 st.write("See the left sidebar for the different pages.")
+st.write('')
+st.markdown("[Source Data](https://electionresults.parliament.uk/general-elections)")
+st.markdown("[My GitHub](https://github.com/phoughton)")
